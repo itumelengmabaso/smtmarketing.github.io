de09b0115e8686ecb123decd3b6587955ce40932
Bruv Weird <thulani.ncube21@yahoo.com>
2019-02-19 09:39:27 +0200

    Redesigned website

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
